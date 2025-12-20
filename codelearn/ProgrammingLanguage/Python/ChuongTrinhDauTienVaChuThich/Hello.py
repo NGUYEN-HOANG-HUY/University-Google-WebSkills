2738124a89d69ad868fd2745e6ac9389cd36934d
Nguyen Hoang Huy <nguyenhuyhoang2k4smart@gmail.com>
2025-12-20 11:18:50 +0700

    Initial commit

--- codelearn/ProgrammingLanguage/Python/ChuongTrinhDauTienVaChuThich/Hello.py
+++ codelearn/ProgrammingLanguage/Python/ChuongTrinhDauTienVaChuThich/Hello.py
@@ -1,1 +1,12 @@
-print("Hello")
+n = int(input())
+
+def is_abundant(n):
+    total = 0
+
+    for i in range(1, n):
+        if n % i == 0:
+            total += i
+
+    return total > n
+
+print(is_abundant(n))

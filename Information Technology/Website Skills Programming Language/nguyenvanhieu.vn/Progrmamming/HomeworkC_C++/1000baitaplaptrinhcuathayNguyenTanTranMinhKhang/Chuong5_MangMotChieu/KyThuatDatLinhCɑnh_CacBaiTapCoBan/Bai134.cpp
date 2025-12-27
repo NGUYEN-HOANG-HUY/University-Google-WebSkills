e2b684a95c5e953a1658b50e7f69c1a4dab33eaf
Nguyen Hoang Huy <nguyenhuyhoang2k4smart@gmail.com>
2025-12-27 11:29:29 +0700

    Initial commit

--- Information Technology/Website Skills Programming Language/nguyenvanhieu.vn/Progrmamming/HomeworkC_C++/1000baitaplaptrinhcuathayNguyenTanTranMinhKhang/Chuong5_MangMotChieu/KyThuatDatLinhCɑnh_CacBaiTapCoBan/Bai134.cpp
+++ Information Technology/Website Skills Programming Language/nguyenvanhieu.vn/Progrmamming/HomeworkC_C++/1000baitaplaptrinhcuathayNguyenTanTranMinhKhang/Chuong5_MangMotChieu/KyThuatDatLinhCɑnh_CacBaiTapCoBan/Bai134.cpp
@@ -1,51 +1,79 @@
-//
-// Created by ASUS on 12/8/2025.
-//
 #include <iostream>
+#include <stdio.h>
 
+#define MAX 100
 using namespace std;
-#define Max 100
 
-void Intput(int a[], int &n) {
-    // Nhập số lượng phần tử
+// Nhập mảng
+void GetInformation(float a[], int &n) {
     do {
-        cout << "Nhap so phan tu n: ";
-        cin >> n;
-        if (n <= 1 || n >= Max) {
-            cout << "Khong hop le, vui long nhap lai!\n";
+        printf("Nhap so luong n: ");
+        scanf("%d", &n);
+        if (n < 1 || n > MAX) {
+            printf("Khong hop le, nhap lai!\n");
         }
-    } while (n <= 1 || n >= Max);
+    } while (n < 1 || n > MAX);
 
-    // Nhập từng phần tử
     for (int i = 0; i < n; i++) {
-        cout << "Nhap a[" << i << "]: ";
-        cin >> a[i];
+        printf("a[%d] = ", i);
+        scanf("%f", &a[i]);
     }
-    cout << endl;
 }
 
-void Output(int a[], int n) {
-    cout << "\nMang vua nhap: ";
+// Xuất mảng
+void OutInformation(float a[], int n) {
     for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
+        printf("%.2f ", a[i]);
     }
-    cout << endl;
+    printf("\n");
 }
 
-int MaxValues(int a[], int n) {
-    int Max = a[0];
+// Tìm giá trị dương đầu tiên
+float findTheFirstPositiveValue(float a[], int n) {
     for (int i = 0; i < n; i++) {
-        if (a[i] > Max)
-            cout << a[i];
+        if (a[i] > 0) {
+            return a[i];
+        }
     }
-    cout << endl;
+    return -1;
 }
-
 
 int main() {
-    int a[Max], n;
-    Intput(a, n);
-    Output(a, n);
-    MaxValues(a, n);
+    float a[MAX];
+    int n = 0;
+    int choice;
+
+    do {
+        printf("\n===== MENU =====\n");
+        printf("1. Nhap mang\n");
+        printf("2. Xuat mang\n");
+        printf("3. Tim so duong dau tien\n");
+        printf("0. Thoat\n");
+        printf("Lua chon: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                GetInformation(a, n);
+                break;
+            case 2:
+                OutInformation(a, n);
+                break;
+            case 3: {
+                float result = findTheFirstPositiveValue(a, n);
+                if (result == -1)
+                    printf("Mang khong co gia tri duong\n");
+                else
+                    printf("Gia tri duong dau tien: %.2f\n", result);
+                break;
+            }
+            case 0:
+                printf("Thoat chuong trinh\n");
+                break;
+            default:
+                printf("Lua chon khong hop le\n");
+        }
+    } while (choice != 0);
+
     return 0;
 }
